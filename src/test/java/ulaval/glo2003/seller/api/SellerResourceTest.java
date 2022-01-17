32f1a1fdf69338ec9d6bada54b3e2d0eb1d2cc3f
maximemvd <maximemvd@hotmail.fr>
2022-01-17 15:39:41 -0500

    add seller test

--- src/test/java/ulaval/glo2003/seller/api/SellerResourceTest.java
+++ src/test/java/ulaval/glo2003/seller/api/SellerResourceTest.java
@@ -3,13 +3,13 @@ package ulaval.glo2003.seller.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ulaval.glo2003.seller.domain.Seller;
 import ulaval.glo2003.seller.service.SellerService;
 
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,15 +35,8 @@ public class SellerResourceTest {
     }
 
     @Test
-    public void givenAnSellerRequest_whenAddSeller_thenShouldCallTheAssembler() {
-        this.sellerResource.createSeller(this.sellerRequest);
-
-        verify(this.sellerAssembler).assembletoInternal(this.sellerRequest);
-    }
-
-    @Test
-    public void givenAnSellerRequest_whenAddSeller_thenShouldCallTheSellerService() {
-        BDDMockito.given(this.sellerAssembler.assembletoInternal(sellerRequest)).willReturn(this.seller);
+    public void givenASellerRequest_whenAddSeller_thenShouldCreateSeller() {
+        given(this.sellerAssembler.assembleToInternal(sellerRequest)).willReturn(this.seller);
 
         this.sellerResource.createSeller(this.sellerRequest);
 
