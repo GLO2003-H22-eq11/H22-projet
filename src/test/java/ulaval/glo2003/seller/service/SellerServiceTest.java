68d2d73108baec43045ba73d80c861a013b932ea
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2022-01-17 12:27:02 -0500

    feat(4):test-service

---
+++ src/test/java/ulaval/glo2003/seller/service/SellerServiceTest.java
@@ -0,0 +1,49 @@
+package ulaval.glo2003.seller.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ulaval.glo2003.seller.domain.Seller;
+import ulaval.glo2003.seller.domain.SellerRepository;
+
+import static org.mockito.Mockito.never;
+
+@ExtendWith(MockitoExtension.class)
+class SellerServiceTest {
+
+    @Mock
+    private Seller seller;
+
+    @Mock
+    private SellerRepository sellerRepository;
+
+    private SellerService sellerService;
+
+    @BeforeEach
+    public void setUp() {
+        this.sellerService = new SellerService(this.sellerRepository);
+    }
+
+    @Test
+    public void givenAMajorSeller_whenAddSeller_thenShouldAddTheSeller() {
+        BDDMockito.given(this.seller.isMajor()).willReturn(true);
+
+        this.sellerService.addSeller(this.seller);
+
+        Mockito.verify(this.sellerRepository).save(this.seller);
+    }
+
+    @Test
+    public void givenAMinorSeller_whenAddSeller_thenShouldAddTheSeller() {
+        BDDMockito.given(this.seller.isMajor()).willReturn(false);
+
+        this.sellerService.addSeller(this.seller);
+
+        Mockito.verify(this.sellerRepository, never()).save(this.seller);
+    }
+
+}
