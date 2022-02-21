3c32dc249cec81c95f0cd2b6ed5c9b53cc2a26cb
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2022-02-21 15:23:05 -0500

    environnement

---
+++ src/test/java/ulaval/glo2003/endtoend/EndToEndTest.java
@@ -0,0 +1,16 @@
+package ulaval.glo2003.endtoend;
+
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class EndToEndTest {
+
+  public void setUp() {
+
+
+  }
+
+}
