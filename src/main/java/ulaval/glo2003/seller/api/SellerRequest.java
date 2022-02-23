7599bc1a24f1c1648cb4a6c53b9d3a395219653f
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2022-02-23 11:54:03 -0500

    feat:33 creation du seller test acceptation

--- src/main/java/ulaval/glo2003/seller/api/SellerRequest.java
+++ src/main/java/ulaval/glo2003/seller/api/SellerRequest.java
@@ -4,12 +4,12 @@ import jakarta.validation.constraints.NotNull;
 import ulaval.glo2003.exception.GenericRequest;
 
 public class SellerRequest extends GenericRequest {
-  @NotNull
-  public String name;
+    @NotNull
+    public String name;
 
-  @NotNull
-  public String bio;
+    @NotNull
+    public String bio;
 
-  @NotNull
-  public String birthDate;
+    @NotNull
+    public String birthDate;
 }
