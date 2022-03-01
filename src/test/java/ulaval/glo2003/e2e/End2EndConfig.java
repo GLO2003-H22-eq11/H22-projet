88d24cbe619249b9eb60b1211a5c3f13ecbe9ed7
Vincent Fournier <vincent.fournier.17@ulaval.ca>
2022-03-01 16:01:08 -0500

    feat(34): Refactor de tests echec

--- src/test/java/ulaval/glo2003/e2e/End2EndConfig.java
+++ src/test/java/ulaval/glo2003/e2e/End2EndConfig.java
@@ -19,6 +19,7 @@ public abstract class End2EndConfig {
   public static final String A_PRODUCT_TITLE = "TITLE";
   public static final String A_PRODUCT_DESCRIPTION = "product description";
   public static final String A_VALID_UUID_FORMAT = "0ce7f24b-e80e-4513-a7bc-78ae2a71c3a5";
+  public static final String A_VALID_NON_EXISTING_UUID_FORMAT = "d3815eac-bf01-49bf-b570-3a8f376b8c15";
   public static final String A_NON_VALID_UUID_FORMAT = "0ce7f24b";
   public static final int A_VALID_SUGGESTED_PRICE = 10;
   public static final List<String> A_CATEGORIES = List.of("A", "B", "C");
