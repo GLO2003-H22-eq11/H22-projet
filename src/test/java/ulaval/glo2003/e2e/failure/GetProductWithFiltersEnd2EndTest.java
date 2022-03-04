cd41479ead2e2dd106029aa6c967a421a96cc36c
marin beauchesne <marin.beauchesne.1@ulaval.ca>
2022-03-04 14:49:52 -0500

    feat(34): ajout test filters

--- src/test/java/ulaval/glo2003/e2e/failure/GetProductWithFiltersEnd2EndTest.java
+++ src/test/java/ulaval/glo2003/e2e/failure/GetProductWithFiltersEnd2EndTest.java
@@ -1,4 +1,24 @@
 package ulaval.glo2003.e2e.failure;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import ulaval.glo2003.ApplicationMain;
+
+import static spark.Spark.stop;
+
 public class GetProductWithFiltersEnd2EndTest {
+
+  @BeforeAll
+  public static void startServer() {
+    try {
+      ApplicationMain.main(new String[0]);
+
+    } catch (Exception ignored) {
+    }
+  }
+
+  @AfterAll
+  public static void stopServer() {
+    stop();
+  }
 }
