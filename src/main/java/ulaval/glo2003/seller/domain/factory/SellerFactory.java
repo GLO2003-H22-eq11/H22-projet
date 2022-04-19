83fcf4d51e342b872626eb688c1f0c8682970e2d
maximemvd <maximemvd@hotmail.fr>
2022-04-19 15:00:13 -0400

    fix: erreurs du tp3

--- src/main/java/ulaval/glo2003/seller/domain/factory/SellerFactory.java
+++ src/main/java/ulaval/glo2003/seller/domain/factory/SellerFactory.java
@@ -2,24 +2,33 @@ package ulaval.glo2003.seller.domain.factory;
 
 import ulaval.glo2003.exception.GenericException;
 import ulaval.glo2003.seller.domain.Seller;
+import ulaval.glo2003.seller.domain.exceptions.SellerIsMinorException;
 import ulaval.glo2003.util.DateParser;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class SellerFactory {
+  private static final int MAJOR_AGE = 18;
   private final SellerIdFactory sellerIdFactory;
 
   public SellerFactory(SellerIdFactory sellerIdFactory) {
     this.sellerIdFactory = sellerIdFactory;
   }
 
-  public Seller create(String name, String bio, String birthDate) throws GenericException {
-    return new Seller(
-            this.sellerIdFactory.create(),
-            name,
-            bio,
-            DateParser.format(birthDate),
-            Instant.now()
-    );
+  public Seller create(String name, String bio, String birthDateString) throws GenericException {
+    LocalDate birthDate = DateParser.format(birthDateString);
+    int age = Period.between(birthDate, LocalDate.now()).getYears();
+    if (age >= MAJOR_AGE) {
+      return new Seller(
+              this.sellerIdFactory.create(),
+              name,
+              bio,
+              birthDate,
+              Instant.now()
+      );
+    }
+    throw new SellerIsMinorException();
   }
 }
