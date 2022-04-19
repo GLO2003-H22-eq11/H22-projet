d066f23b395717019b8c9e12b99d4c27ce441663
maximemvd <maximemvd@hotmail.fr>
2022-04-19 15:09:04 -0400

    code review

--- src/main/java/ulaval/glo2003/seller/domain/factory/SellerFactory.java
+++ src/main/java/ulaval/glo2003/seller/domain/factory/SellerFactory.java
@@ -3,11 +3,11 @@ package ulaval.glo2003.seller.domain.factory;
 import ulaval.glo2003.exception.GenericException;
 import ulaval.glo2003.seller.domain.Seller;
 import ulaval.glo2003.seller.domain.exceptions.SellerIsMinorException;
+import ulaval.glo2003.util.AgeVerificator;
 import ulaval.glo2003.util.DateParser;
 
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.Period;
 
 public class SellerFactory {
   private static final int MAJOR_AGE = 18;
@@ -19,16 +19,17 @@ public class SellerFactory {
 
   public Seller create(String name, String bio, String birthDateString) throws GenericException {
     LocalDate birthDate = DateParser.format(birthDateString);
-    int age = Period.between(birthDate, LocalDate.now()).getYears();
-    if (age >= MAJOR_AGE) {
-      return new Seller(
-              this.sellerIdFactory.create(),
-              name,
-              bio,
-              birthDate,
-              Instant.now()
-      );
+
+    if (AgeVerificator.isYoungerThanAge(birthDate, MAJOR_AGE)) {
+      throw new SellerIsMinorException();
     }
-    throw new SellerIsMinorException();
+
+    return new Seller(
+            this.sellerIdFactory.create(),
+            name,
+            bio,
+            birthDate,
+            Instant.now()
+    );
   }
 }
